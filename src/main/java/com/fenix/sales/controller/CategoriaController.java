07cc04d2aa098182c20627b56f1c520d667c42e2
edayane <edaytavares@gmail.com>
2022-10-06 21:18:39 -0300

    funcionalidade categoria

---
+++ src/main/java/com/fenix/sales/controller/CategoriaController.java
@@ -0,0 +1,91 @@
+package com.fenix.sales.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.fenix.sales.entity.Categoria;
+import com.fenix.sales.infra.FileSaver;
+import com.fenix.sales.repository.CategoriaRepository;
+
+
+@Controller
+public class CategoriaController {
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	@Autowired
+	FileSaver fileSaver;
+	
+	@GetMapping("/admin/categoria")
+	public ModelAndView form(Categoria categoria) {
+		ModelAndView modelAndView = new ModelAndView("admin/formCategoria");
+		return modelAndView;
+	}
+	
+	@PostMapping("/admin/cadastarcategoria")
+	public ModelAndView create(@Valid Categoria categoria, BindingResult bindingResult, RedirectAttributes redirectAtributes) {
+		ModelAndView modelAndView = new ModelAndView("redirect:/admin/formCategoria");
+		
+		if(bindingResult.hasErrors()) {
+			return form(categoria);
+		}
+					
+		System.out.println(categoria);
+		categoriaRepository.save(categoria);
+		redirectAtributes.addFlashAttribute("create", categoria.getDescricao() + " Categoria cadastrada com sucesso!!!");
+		return modelAndView;
+	}
+	
+	@GetMapping("/admin/listcategorias")
+	public ModelAndView listarCategorias() {
+		ModelAndView modelAndView = new ModelAndView("admin/listCategorias");
+		List<Categoria> categorias = categoriaRepository.findAll();
+		modelAndView.addObject("sacolaCategoria", categorias);
+		System.out.println("listar Categorias");
+		return modelAndView;
+	}
+	@GetMapping("/excluircategoria/{id}")
+	public ModelAndView excluirCategorias(@PathVariable("id") Integer id, RedirectAttributes redirectAttributes) {
+		ModelAndView modelAndView = new ModelAndView("redirect:/admin/listcategorias");
+		Categoria categoria = categoriaRepository.findById(id).get();
+		categoriaRepository.delete(categoria);
+		redirectAttributes.addFlashAttribute("Mensagem", "Categoria excluida com sucesso");
+		System.out.println("Excluiu a categoria");
+		return modelAndView;
+	}
+
+	@GetMapping("/editarcategoria/{id}")
+	public ModelAndView editar(@PathVariable("id") Integer id, Categoria categoria, RedirectAttributes redirectAttributes) {
+		ModelAndView modelAndView = new ModelAndView("admin/categoriaAtualizar");
+		categoria = categoriaRepository.getById(id);
+		System.out.println(categoria);
+		modelAndView.addObject("categ", categoria);
+		return modelAndView;
+	}
+
+	@PostMapping("/editarcategoria")
+	public ModelAndView update (@Valid Categoria categoria, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+		ModelAndView modelAndView = new ModelAndView("redirect:/admin/listcategorias");
+		
+		if(bindingResult.hasErrors()) {
+			return editar(categoria.getId(), categoria, redirectAttributes);
+		} 
+		
+		Categoria categ1 = categoriaRepository.getById(categoria.getId());
+		categ1.setDescricao(categoria.getDescricao()) ;
+		
+		categoriaRepository.save(categ1);
+		return modelAndView;
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/admin/formCategoria.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
+<!-- Import da taglib -->
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="s"%>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Formulário</title>
+
+<link rel = "Stylesheet" href = "/resources/css/formCategoria.css">
+
+</head>
+
+<body>
+</body>
+</html>
