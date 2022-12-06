c65b3552592b661aaa30bb1e49f5b3f9c1659a35
joabsonSants <j.joabson.santos@gmail.com>
2022-12-05 22:18:53 -0300

    semeando banco com SQL

---
+++ src/main/resources/import.sql
@@ -0,0 +1,31 @@
+INSERT INTO TBL_AUTORES(id,nome,status) VALUES (1,'Primo Rico','true');
+INSERT INTO TBL_AUTORES(id,nome,status) VALUES (2,'Jose Alfredo','true');
+INSERT INTO TBL_AUTORES(id,nome,status) VALUES (3,'ALberto','true');
+INSERT INTO TBL_AUTORES(id,nome,status) VALUES (4,'Luiz henrique','true');
+INSERT INTO TBL_AUTORES(id,nome,status) VALUES (5,'Carlos drumont','true');
+
+INSERT INTO TBL_EDITORA(id,descricao,status) VALUES (1,'saraiva','true');
+INSERT INTO TBL_EDITORA(id,descricao,status) VALUES (2,'moderna','true');
+INSERT INTO TBL_EDITORA(id,descricao,status) VALUES (3,'roma','true');
+INSERT INTO TBL_EDITORA(id,descricao,status) VALUES (4,'global','true');
+INSERT INTO TBL_EDITORA(id,descricao,status) VALUES (5,'brasil','true');
+
+INSERT INTO TBL_CATEGORIAS(id,nome,status) VALUES (1,'romance','true');
+INSERT INTO TBL_CATEGORIAS(id,nome,status) VALUES (2,'financas','true');
+INSERT INTO TBL_CATEGORIAS(id,nome,status) VALUES (3,'esportes','true');
+INSERT INTO TBL_CATEGORIAS(id,nome,status) VALUES (4,'desenvolvimento','true');
+INSERT INTO TBL_CATEGORIAS(id,nome,status) VALUES (5,'tecnologia','true');
+
+INSERT INTO TBL_LIVROS(ID,DESTAQUE,FOTO,PAGINAS,PRECO,STATUS,TITULO,AUTOR_ID,CATEGORIA_ID,EDITORA_ID,DESCRICAO) VALUES (1,'true','',45,100.0,'true','do mil ao milhao',1,1,1,'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown');
+
+INSERT INTO TBL_LIVROS(ID,DESTAQUE,FOTO,PAGINAS,PRECO,STATUS,TITULO,AUTOR_ID,CATEGORIA_ID,EDITORA_ID,DESCRICAO) VALUES (2,'true','',85,200.0,'true','O Segredo Da Mente Milionaria',1,2,3,'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown');
+
+INSERT INTO TBL_LIVROS(ID,DESTAQUE,FOTO,PAGINAS,PRECO,STATUS,TITULO,AUTOR_ID,CATEGORIA_ID,EDITORA_ID,DESCRICAO) VALUES (3,'true','',100,300.0,'true','O Poder Do Subconsciente',1,2,4,'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown');
+
+INSERT INTO TBL_LIVROS(ID,DESTAQUE,FOTO,PAGINAS,PRECO,STATUS,TITULO,AUTOR_ID,CATEGORIA_ID,EDITORA_ID,DESCRICAO) VALUES (4,'true','',76,400.0,'true','Liberte O Poder Do Seu Subconsciente',4,2,1,'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown');
+
+INSERT INTO TBL_LIVROS(ID,DESTAQUE,FOTO,PAGINAS,PRECO,STATUS,TITULO,AUTOR_ID,CATEGORIA_ID,EDITORA_ID,DESCRICAO) VALUES (5,'true','',500,500.0,'true','A Arte De Conquistar Superpoderes Mentais',1,3,2,'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown');
+
+INSERT INTO TBL_LIVROS(ID,DESTAQUE,FOTO,PAGINAS,PRECO,STATUS,TITULO,AUTOR_ID,CATEGORIA_ID,EDITORA_ID,DESCRICAO) VALUES (6,'true','',175,600.0,'true','mister musca',2,2,2,'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown');
+
+ 
